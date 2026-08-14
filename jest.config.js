172c991485ef54125e10bda50a0969e83bf6bb7b
sumingdashuaige <2697617354@qq.com>
2026-08-14 19:19:35 +0800

    chore: add jest-expo test infrastructure

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFiles: ['./jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|react-native-reanimated)',
+  ],
+};
